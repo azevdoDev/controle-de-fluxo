a6c39985f539ee832c74bd6a6a4c4443a48b6339
azevdoDev <contato.azevedo@outlook.com.br>
2024-07-24 09:47:20 -0300

    CriandoExcecoes

---
+++ src/FormatadorCep.java
@@ -0,0 +1,21 @@
+public class FormatadorCep {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("2376506");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            // TODO Auto-generated catch block
+            System.out.println("O Cep deve ter 8 dígitos");
+        }
+
+    }
+
+    static String formatarCep(String cep) throws CepInvalidoException {
+        if (cep.length() != 8)
+            throw new CepInvalidoException();
+
+        // simula um cep formatado
+        return "23.765-064";
+    }
+
+}
